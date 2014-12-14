13f9a8e9058edd9f1d8b53e8121c3a713a186ad1
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2014-12-14 16:12:41 +0100

    Fixed #7 and #3

--- src/Crypto/RNCryptor/V3.hs
+++ src/Crypto/RNCryptor/V3.hs
@@ -2,7 +2,9 @@
 module Crypto.RNCryptor.V3 (
     module Crypto.RNCryptor.V3.Encrypt
   , module Crypto.RNCryptor.V3.Decrypt
+  , module Crypto.RNCryptor.Types
   ) where
 
 import Crypto.RNCryptor.V3.Encrypt
 import Crypto.RNCryptor.V3.Decrypt
+import Crypto.RNCryptor.Types
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,3 +1,23 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Tests where
 
-import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Crypto.RNCryptor.V3
+import Control.Applicative
+import qualified Data.ByteString as B
+
+
+newtype TestVector = TV (UserInput, UserInput, RNCryptorHeader) deriving Show
+
+instance Arbitrary TestVector where
+  arbitrary = TV <$> ((,,) <$> arbitrary <*> arbitrary <*> arbitrary)
+
+
+testEncryptDecryptRoundtrip :: Property
+testEncryptDecryptRoundtrip =
+  forAll arbitrary $ \(TV (input,pwd,hdr)) ->
+    B.length (unInput input) > 0 &&
+    B.length (unInput pwd) > 0 ==>
+    let ctx = newRNCryptorContext (unInput pwd) hdr
+        encrypted = encrypt ctx (unInput input)
+    in decrypt encrypted (unInput pwd) == unInput input
